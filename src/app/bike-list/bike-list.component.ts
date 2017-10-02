ca32834ff16c8406491b48886618f47c7ad9c0e9
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2017-10-02 19:04:36 +0100

    broke version

--- src/app/bike-list/bike-list.component.ts
+++ src/app/bike-list/bike-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IBike } from '../bike';
+import { $ } from 'protractor';
+// angular.module('bike', []);
 
 @Component({
     selector: 'app-bike-list',
@@ -21,80 +23,80 @@ export class BikeListComponent implements OnInit {
         this.filteredBikes = this.listFilter ? this.performFilter(this.listFilter) : this.bikes;
     }
     filteredBikes: IBike[];
-    bikes: IBike[] =
-    [
-        {
-          'bikeId': 1,
-          'bikeName': 'MTB bike',
-          'bikeCode': 'MTB-0020',
-          'releaseDate': 'March 18, 2016',
-          'description': 'Mens 29 inch mountain bike',
-          'category': 'mens',
-          'price': 532.99,
-          'starRating': 4.2,
-          'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177215/bike-mountain.png'
-        },
-        {
-          'bikeId': 2,
-          'bikeName': 'MTB bike',
-          'bikeCode': 'MTB-0030',
-          'releaseDate': 'June 15, 2017',
-          'description': 'Womans 27 inch mountain bike',
-          'category': 'womens',
-          'price': 515.99,
-          'starRating': 4.5,
-          'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177215/bike-mountain.png'
-        },
-        {
-          'bikeId': 3,
-          'bikeName': 'Sports bike',
-          'bikeCode': 'SB-1020',
-          'releaseDate': 'May 9, 2016',
-          'description': 'Mens Sports Bike',
-          'category': 'mens',
-          'price': 1099.99,
-          'starRating': 4.8,
-          'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177216/bike-sport.png'
-        },
-        {
-          'bikeId': 4,
-          'bikeName': 'Kids bike',
-          'bikeCode': 'KB-3045',
-          'releaseDate': 'October 28, 2016',
-          'description': 'Kids bike with stabalizers',
-          'category': 'kids',
-          'price': 250.00,
-          'starRating': 4.4,
-          'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177214/bike-kid.png'
-        },
-        {
-          'bikeId': 5,
-          'bikeName': 'Ladies bike',
-          'bikeCode': 'LB-4060',
-          'releaseDate': 'March 10, 2016',
-          'description': 'Ladies touring bike',
-          'category': 'womens',
-          'price': 450.00,
-          'starRating': 4.9,
-          'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177213/bike-female.png'
-        },
-        {
-          'bikeId': 6,
-          'bikeName': 'Kids blue bike',
-          'bikeCode': 'KB-3075',
-          'releaseDate': 'June 28, 2016',
-          'description': 'Kids blue bike with stabalizers',
-          'category': 'kids',
-          'price': 290.00,
-          'starRating': 4.8,
-          'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177214/bike-kid.png'
-        }
-      ];
-    
+    bikes: IBike[] ;
+    // [
+    //     {
+    //       'bikeId': 1,
+    //       'bikeName': 'MTB bike',
+    //       'bikeCode': 'MTB-0020',
+    //       'releaseDate': 'March 18, 2016',
+    //       'description': 'Mens 29 inch mountain bike',
+    //       'category': 'mens',
+    //       'price': 532.99,
+    //       'starRating': 4.2,
+    //       'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177215/bike-mountain.png'
+    //     },
+    //     {
+    //       'bikeId': 2,
+    //       'bikeName': 'MTB bike',
+    //       'bikeCode': 'MTB-0030',
+    //       'releaseDate': 'June 15, 2017',
+    //       'description': 'Womans 27 inch mountain bike',
+    //       'category': 'womens',
+    //       'price': 515.99,
+    //       'starRating': 4.5,
+    //       'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177215/bike-mountain.png'
+    //     },
+    //     {
+    //       'bikeId': 3,
+    //       'bikeName': 'Sports bike',
+    //       'bikeCode': 'SB-1020',
+    //       'releaseDate': 'May 9, 2016',
+    //       'description': 'Mens Sports Bike',
+    //       'category': 'mens',
+    //       'price': 1099.99,
+    //       'starRating': 4.8,
+    //       'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177216/bike-sport.png'
+    //     },
+    //     {
+    //       'bikeId': 4,
+    //       'bikeName': 'Kids bike',
+    //       'bikeCode': 'KB-3045',
+    //       'releaseDate': 'October 28, 2016',
+    //       'description': 'Kids bike with stabalizers',
+    //       'category': 'kids',
+    //       'price': 250.00,
+    //       'starRating': 4.4,
+    //       'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177214/bike-kid.png'
+    //     },
+    //     {
+    //       'bikeId': 5,
+    //       'bikeName': 'Ladies bike',
+    //       'bikeCode': 'LB-4060',
+    //       'releaseDate': 'March 10, 2016',
+    //       'description': 'Ladies touring bike',
+    //       'category': 'womens',
+    //       'price': 450.00,
+    //       'starRating': 4.9,
+    //       'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177213/bike-female.png'
+    //     },
+    //     {
+    //       'bikeId': 6,
+    //       'bikeName': 'Kids blue bike',
+    //       'bikeCode': 'KB-3075',
+    //       'releaseDate': 'June 28, 2016',
+    //       'description': 'Kids blue bike with stabalizers',
+    //       'category': 'kids',
+    //       'price': 290.00,
+    //       'starRating': 4.8,
+    //       'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177214/bike-kid.png'
+    //     }
+    //   ];
 
-     constructor(){
-         this.filteredBikes= this.bikes;
-     }
+
+    constructor() {
+        this.filteredBikes = this.bikes;
+    }
     performFilter(filterBy: string): IBike[] {
         filterBy = filterBy.toLocaleLowerCase();
         return this.bikes.filter((bike: IBike) =>
@@ -109,4 +111,96 @@ export class BikeListComponent implements OnInit {
         console.log('In OnInit');
     }
 
+     TypeCtrl($bike) 
+    {
+        $bike = [];
+
+        $bike.bikeIncludes = [
+            {
+                'bikeId': 1,
+                'bikeName': 'MTB bike',
+                'bikeCode': 'MTB-0020',
+                'releaseDate': 'March 18, 2016',
+                'description': 'Mens 29 inch mountain bike',
+                'category': 'mens',
+                'price': 532.99,
+                'starRating': 4.2,
+                'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177215/bike-mountain.png'
+            },
+            {
+                'bikeId': 2,
+                'bikeName': 'MTB bike',
+                'bikeCode': 'MTB-0030',
+                'releaseDate': 'June 15, 2017',
+                'description': 'Womans 27 inch mountain bike',
+                'category': 'womens',
+                'price': 515.99,
+                'starRating': 4.5,
+                'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177215/bike-mountain.png'
+            },
+            {
+                'bikeId': 3,
+                'bikeName': 'Sports bike',
+                'bikeCode': 'SB-1020',
+                'releaseDate': 'May 9, 2016',
+                'description': 'Mens Sports Bike',
+                'category': 'mens',
+                'price': 1099.99,
+                'starRating': 4.8,
+                'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177216/bike-sport.png'
+            },
+            {
+                'bikeId': 4,
+                'bikeName': 'Kids bike',
+                'bikeCode': 'KB-3045',
+                'releaseDate': 'October 28, 2016',
+                'description': 'Kids bike with stabalizers',
+                'category': 'kids',
+                'price': 250.00,
+                'starRating': 4.4,
+                'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177214/bike-kid.png'
+            },
+            {
+                'bikeId': 5,
+                'bikeName': 'Ladies bike',
+                'bikeCode': 'LB-4060',
+                'releaseDate': 'March 10, 2016',
+                'description': 'Ladies touring bike',
+                'category': 'womens',
+                'price': 450.00,
+                'starRating': 4.9,
+                'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177213/bike-female.png'
+            },
+            {
+                'bikeId': 6,
+                'bikeName': 'Kids blue bike',
+                'bikeCode': 'KB-3075',
+                'releaseDate': 'June 28, 2016',
+                'description': 'Kids blue bike with stabalizers',
+                'category': 'kids',
+                'price': 290.00,
+                'starRating': 4.8,
+                'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177214/bike-kid.png'
+            }
+        ];
+
+        $bike.includeName = function (name) {
+            var i = $bike.inArray(name, $bike.bikeIncludes);
+            if (i > -1) {
+                $bike.colourIncludes.splice(i, 1);
+            } else {
+                $bike.colourIncludes.push(name);
+            }
+        }
+
+        $bike.typeFilter = function (bike) {
+            if ($bike.bikeIncludes.length > 0) {
+                if ($bike.inArray(bike.category, $bike.bikeIncludes) < 0)
+                    return;
+            }
+
+            return bike;
+        }
+    }
+    
 }
