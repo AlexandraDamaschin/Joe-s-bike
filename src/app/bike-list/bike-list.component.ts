a3b78f2346ef70196ba7739f4e0cdc4bb125fdc9
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2017-10-03 07:46:12 +0100

    no more .json

--- src/app/bike-list/bike-list.component.ts
+++ src/app/bike-list/bike-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IBike } from '../bike';
-import { $ } from 'protractor';
+import * as _ from 'lodash';
+// import { $ } from 'protractor';
 // angular.module('bike', []);
 
 @Component({
@@ -9,62 +10,130 @@ import { $ } from 'protractor';
     styleUrls: ['./bike-list.component.css']
 })
 export class BikeListComponent implements OnInit {
-    pageTitle: string = "Joe`s bikes";
+    pageTitle: string = "Joe`s Bikes";
     imageWidth: number = 50;
     imageMargin: number = 2;
-    showImage: boolean = true;
 
-    _listFilter: string;
-    get listFilter(): string {
-        return this._listFilter;
-    }
-    set listFilter(value: string) {
-        this._listFilter = value;
-        this.filteredBikes = this.listFilter ? this.performFilter(this.listFilter) : this.bikes;
-    }
+    _showImage: boolean = true;
+    _showMens: boolean = true;
+    _showWomens: boolean = true;
+    _showKids: boolean = true;
+
     filteredBikes: IBike[];
-    bikes: IBike[] ;
+    bikes: IBike[] =
+    [
+        {
+            'bikeId': 1,
+            'bikeName': 'MTB bike',
+            'bikeCode': 'MTB-0020',
+            'releaseDate': 'March 18, 2016',
+            'description': 'Mens 29 inch mountain bike',
+            'category': 'mens',
+            'price': 532.99,
+            'starRating': 4.2,
+            'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177215/bike-mountain.png'
+        },
+        {
+            'bikeId': 2,
+            'bikeName': 'MTB bike',
+            'bikeCode': 'MTB-0030',
+            'releaseDate': 'June 15, 2017',
+            'description': 'Womans 27 inch mountain bike',
+            'category': 'womens',
+            'price': 515.99,
+            'starRating': 4.5,
+            'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177215/bike-mountain.png'
+        },
+        {
+            'bikeId': 3,
+            'bikeName': 'Sports bike',
+            'bikeCode': 'SB-1020',
+            'releaseDate': 'May 9, 2016',
+            'description': 'Mens Sports Bike',
+            'category': 'mens',
+            'price': 1099.99,
+            'starRating': 4.8,
+            'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177216/bike-sport.png'
+        },
+        {
+            'bikeId': 4,
+            'bikeName': 'Kids bike',
+            'bikeCode': 'KB-3045',
+            'releaseDate': 'October 28, 2016',
+            'description': 'Kids bike with stabalizers',
+            'category': 'kids',
+            'price': 250.00,
+            'starRating': 4.4,
+            'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177214/bike-kid.png'
+        },
+        {
+            'bikeId': 5,
+            'bikeName': 'Ladies bike',
+            'bikeCode': 'LB-4060',
+            'releaseDate': 'March 10, 2016',
+            'description': 'Ladies touring bike',
+            'category': 'womens',
+            'price': 450.00,
+            'starRating': 4.9,
+            'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177213/bike-female.png'
+        },
+        {
+            'bikeId': 6,
+            'bikeName': 'Kids blue bike',
+            'bikeCode': 'KB-3075',
+            'releaseDate': 'June 28, 2016',
+            'description': 'Kids blue bike with stabalizers',
+            'category': 'kids',
+            'price': 290.00,
+            'starRating': 4.8,
+            'imageUrl': 'https://openclipart.org/image/2400px/svg_to_png/177214/bike-kid.png'
+        }
+    ];
 
     constructor() {
-        this.filteredBikes = this.bikes;
-    }
-    performFilter(filterBy: string): IBike[] {
-        filterBy = filterBy.toLocaleLowerCase();
-        return this.bikes.filter((bike: IBike) =>
-            bike.bikeName.toLocaleLowerCase().indexOf(filterBy) != -1);
+        this.filteredBikes = this.bikes = [];
     }
 
+    get showMens(): boolean {
+        return this._showMens
+    }
+    set showMens(value: boolean) {
+        this._showMens = value;
+        this.performFilter("mens", value);
+    }
+    get showWomens(): boolean {
+        return this._showWomens;
+    }
+    set showWomens(value: boolean) {
+        this._showWomens = value;
+        this.performFilter("womens", value);
+    }
+    get showKids(): boolean {
+        return this._showKids;
+    }
+    set showKids(value: boolean) {
+        this._showKids = value;
+        this.performFilter("kids", value);
+    }
     toggleImage(): void {
 
-        this.showImage = !this.showImage;
+        this._showImage = !this._showImage;
+    }
+    performFilter(type: string, add: boolean) {
+        if (add) {
+            this.bikes.forEach((bike: IBike) => {
+                if (bike.category.toLowerCase() == type.toLowerCase())
+                    this.filteredBikes.push(bike);
+            });
+        }
+        else {
+            this.filteredBikes = _.filter(this.filteredBikes, (bike: IBike) => {
+                return bike.category.toLowerCase() != type.toLowerCase();
+            });
+        }
+
     }
     ngOnInit(): void {
-        console.log('In OnInit');
     }
-
-     TypeCtrl($bike) 
-    {
-        $bike = [];
-
-        $bike.bikeIncludes = [];
-
-        $bike.includeName = function (name) {
-            var i = $bike.inArray(name, $bike.bikeIncludes);
-            if (i > -1) {
-                $bike.bikeIncludes.splice(i, 1);
-            } else {
-                $bike.bikeIncludes.push(name);
-            }
-        }
-
-        $bike.typeFilter = function (bike) {
-            if ($bike.bikeIncludes.length > 0) {
-                if ($bike.inArray(bike.category, $bike.bikeIncludes) < 0)
-                    return;
-            }
-
-            return bike;
-        }
-    }
-    
 }
+  
